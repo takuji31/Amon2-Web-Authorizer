c58f3bf835cb57e98e5f2b165c79830366486853
Nishibayashi Takuji <takuji31@gmail.com>
2012-12-04 15:33:32 +0900

    add config module skelton

---
+++ lib/Amon2/Web/Authorizer/Config.pm
@@ -0,0 +1,10 @@
+package Amon2::Web::Authorizer::Config;
+use 5.012_001;
+use warnings;
+use utf8;
+
+sub get_config {
+    die "Please override get_config method";
+}
+
+1;
---
+++ lib/Amon2/Web/Authorizer/Config/List.pm
@@ -0,0 +1,14 @@
+package Amon2::Web::Authorizer::Config::List;
+use strict;
+use warnings;
+use utf8;
+use 5.012_001;
+
+use parent qw/Amon2::Web::Authorizer::Config/;
+
+sub get_config {
+    my ($class, $pkg, $opts) = @_;
+    
+}
+
+1;
